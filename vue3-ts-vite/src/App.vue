2ed43084daef978cd8b6bee0af42b96e4f95bd8f
zenquan <1847867150@qq.com>
2022-01-28 16:15:06 +0800

    init: ini tprojects

--- vue3-ts-vite/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <transition name="fade">
-      <router-view :key="$route.path" />
-    </transition>
-  </div>
-</template>
--- vue3-ts-vite/src/pages/NotFound/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="not-found">Not Found</div>
-</template>
-<script>
-export default {
-  name: 'NotFound'
-}
-</script>
